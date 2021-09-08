7ec04973c7bb7682b3f9da08345d22f316568a4c
Dave <daveg7lee@gmail.com>
2021-09-08 19:18:00 +0900

    [FIX] Send only Demerit

--- src/Score/SearchScore/SearchScore.resolvers.ts
+++ src/Score/SearchScore/SearchScore.resolvers.ts
@@ -2,10 +2,12 @@ import client from '../../client';
 
 export default {
   Query: {
-    searchScore: async (_, { term }) =>
-      client.score.findMany({
-        where: { uploader: term },
+    searchScore: async (_, { term }) => {
+      console.log(term);
+      return client.score.findMany({
+        where: { uploader: { contains: term }, type: 'Demerit' },
         include: { user: true },
-      }),
+      });
+    },
   },
 };
