19f2aea676d435a34489b0436f901c474d30f069
Dave <daveg7lee@gmail.com>
2021-10-11 19:42:35 +0900

    [DELETE] Email is not unique

--- src/User/createAccount/createAccount.resolvers.ts
+++ src/User/createAccount/createAccount.resolvers.ts
@@ -12,16 +12,11 @@ export default {
           username,
         },
       });
-      const emailExists = await client.user.findUnique({
-        where: {
-          email,
-        },
-      });
+
       if (usernameExists) {
         throw Error('This username is already taken');
-      } else if (emailExists) {
-        throw Error('This Email already have account');
       }
+
       const encryptedPassword: string = jwt.sign(
         { password },
         process.env.JWT_SECRET
